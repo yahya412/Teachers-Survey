94342f6fcf46265595e5a00a9aa63a867ef022a5
yahya412 <yahya412@yahoo.com>
2021-01-20 03:12:05 +0300

    edit1

--- resources/views/summary/show.blade.php
+++ resources/views/summary/show.blade.php
@@ -8,8 +8,33 @@
 
 
     <div class="container-fluid">
+    <div class="block mb-8">
+                <a href="{{route('summary.index')}}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Main Page</a>
+            </div>
     <div class="row ">
         <div class="col-6 col-md-6 col-sm-6" style="width:5%; display:inline-table;" >
+        <table style="width:25% " class="table table-striped table-bordered table-res">
+  <tr>
+    
+    <th>Questions</th>
+    
+   </tr>   
+
+   <tr>
+   <td>يعرض الاستاذ المراجع الاساسية للمقرر على الطلاب في بداية الفصل الدراسي؟</td>
+   <td>يعرض الاستاذ اهداف المقرر على الطلاب في أول محاضرة؟</td>
+   <td>ينفذ الاستاذ خطة المقرر كما خطط لها في بداية الفصل الدراسي؟</td>
+   <td>تبداء المحاضرة وتنتهي في موعدها المحدد؟</td>
+   <td>طريقة تدريس المقرر تحفز الطلاب على المشاركة وابداء الرأي؟</td>
+   <td>تتناسب اسئلة الاختبارات مع طريقة وأسلوب المدرس؟</td>
+   <td>يسمح استاذ المقرر بمراجعة الطلاب لنتائج اختبارتهم؟</td>
+   <td>استاذ المقرر متمكن من مادة العلمية؟</td>
+   <td>يبداء استاذ المقرر وينتهي بنفس الدرجة من النشاط والحيوية؟</td>
+   <td>اشعر بالرضى بشكل عام على استاذ المقرر؟</td>
+   
+   </tr>
+       </table>
+       
         <table style="width:25% " class="table table-striped table-bordered table-res">
   <tr>
     
